a9c2ce175fa4d78c727cacab6800791c0e9aae25
DaviR77 <davirenatomorais@gmail.com>
2026-09-21 20:00:48 -0300

    Add comments to ResetAdministrador class
    Added comments explaining the purpose of the ResetAdministrador class.

--- src/main/java/com/teatro/ResetAdministrador.java
+++ src/main/java/com/teatro/ResetAdministrador.java
@@ -3,6 +3,8 @@ package com.teatro;
 import com.teatro.model.Administrador;
 import com.teatro.service.AdministradorService;
 
+// Classe para deletar um administrador cadastrado, uma vez que o sistema foi planejado para ser monousuário
+// Executando esta classe isolada, será permitido inicializar o sistema a partir do (novo) cadastro de um administrador
 public class ResetAdministrador {
 
     public static void main(String[] args) {
